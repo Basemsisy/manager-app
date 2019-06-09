81a6a355653e55f832013d1d2e1b6477721cbaeb
basem elsisy <b.elsisy@gmail.com>
2019-06-09 13:58:35 +0200

    add send message feature

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import {connect} from 'react-redux';
+import Communications from 'react-native-communications';
 import {employeeUpdate, employeeSave} from '../actions';
 import {Card, CardSection, Button} from './common';
 import EmployeeForm from './EmployeeForm';
@@ -15,13 +16,24 @@ class EmployeeEdit extends Component {
     const {name, phone, shift} = this.props;
     this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid})
   }
+
+  onTextPress = () => {
+    const {name, phone, shift} = this.props;
+    Communications.text(phone, ` hello ${name} your upcoming shift is on ${shift}`)
+  }
   render () {
     return (
       <Card>
+
         <EmployeeForm/>
         <CardSection>
           <Button onPress={this.onButtonPress}>Save Changes</Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={this.onTextPress}>Send Meesage</Button>
+        </CardSection>
+
       </Card>
     )
   }
